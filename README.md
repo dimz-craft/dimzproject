ba461a9e3f9e32a07f84aaffa3b6ebdaaf7544d1
dimz-craft <dimasbagussaputra112@gmail.com>
2024-06-03 16:36:06 +0700

    add backup link

---
+++ README.md
@@ -0,0 +1,46 @@
+# Dimz Project (private only)
+
+### Another UI
+
+- [v2.0.0](https://dimz-project.vercel.app/download/another-ui/200-90070602-9b65-425c-a642-f93e9d239fc2)
+
+### Available Inventory Viewer
+
+- [v0.0.1](https://dimz-project.vercel.app/download/available-inventory-viewer/001-7446b9bf-778a-4174-a52a-77d8a6f56162)
+- [v0.0.2](https://dimz-project.vercel.app/download/available-inventory-viewer/002-89452cd5-a864-4e67-a93c-c32219e52c61)
+
+### Customizable Days Played
+
+- [v0.0.1](https://dimz-project.vercel.app/download/customizable-days-played/001-b7422e98-c6d4-4a8f-9ba0-6b036d82a21c)
+- [v0.0.2](https://dimz-project.vercel.app/download/customizable-days-played/002-94d188c1-31a7-4476-82b2-5b614bda038c)
+- [v0.0.3](https://dimz-project.vercel.app/download/customizable-days-played/003-94d188c1-31a7-4476-82b2-5b614bda038c)
+
+### Day Counter Addon
+
+- [v0.0.3](https://dimz-project.vercel.app/download/day-counter-addon/003-df60c0c8-950c-428e-956c-6b4c6e9e3097)
+- [v0.0.4](https://dimz-project.vercel.app/download/day-counter-addon/004-aa390588-c865-4377-948d-c80220e1dc46)
+- [v0.0.5](https://dimz-project.vercel.app/download/day-counter-addon/005-8d594e54-f2ef-4c26-8254-c1a9dfbd75b1)
+
+### Durability Armor Viewer
+
+- [v0.0.1](https://dimz-project.vercel.app/download/durability-armor-viewer/001-fbe71cae-24a5-4200-b80f-ec9246af3dcd)
+
+### Durability Tools Viewer
+
+- [v0.0.3](https://dimz-project.vercel.app/download/durability-tools-viewer/003-0c641a1b-34a6-453d-9901-1cb962c76b27)
+
+### F1 Button 
+
+- [v0.0.3](https://dimz-project.vercel.app/download/f1-button/003-67246598-53a1-412f-a466-2d96ef1941a1)
+
+### Nether Coordinate
+
+- [v0.0.1](https://dimz-project.vercel.app/download/nether-coordinate/001-995013db-ba7d-47ed-b248-14a2738466c2)
+
+### Ore Info Viewer
+
+- [v0.0.2](https://dimz-project.vercel.app/download/ore-info-viewer/002-d199ebd9-3b8e-4048-b440-7a4f8ea6ca8a)
+
+### Unobstructive Item Name
+
+- [v0.0.1](https://dimz-project.vercel.app/download/unobstructive-item-name/001-c2f3340b-fa18-43dd-a11a-e42bbc6ed025)
---
+++ src/pages/download/downloadLink.js
@@ -0,0 +1,297 @@
+const downloadLink = [
+  // Another UI
+  {
+    id: "/download/another-ui/200-90070602-9b65-425c-a642-f93e9d239fc2",
+    fileName: "Another UI v2.0.0",
+    filePath: "/download/AnotherUi/v200.mcpack",
+    formatFile: "mcpack",
+  },
+
+  // Available Inventory Viewer
+  {
+    id: "/download/available-inventory-viewer/001-7446b9bf-778a-4174-a52a-77d8a6f56162",
+    fileName: "Available Inventory Viewer v0.0.1",
+    filePath: "/download/AvailableInventoryViewer/v001.mcpack",
+    formatFile: "mcpack",
+  },
+  {
+    id: "/download/available-inventory-viewer/002-89452cd5-a864-4e67-a93c-c32219e52c61",
+    fileName: "Available Inventory Viewer v0.0.2",
+    filePath: "/download/AvailableInventoryViewer/v002.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Customizable Days Played
+  {
+    id: "/download/customizable-days-played/001-b7422e98-c6d4-4a8f-9ba0-6b036d82a21c",
+    fileName: "Customizable Days Played v0.0.1",
+    filePath: "/download/CustomizableDaysPlayed/v001.mcpack",
+    formatFile: "mcpack",
+  },
+  {
+    id: "/download/customizable-days-played/002-94d188c1-31a7-4476-82b2-5b614bda038c",
+    fileName: "Customizable Days Played v0.0.2",
+    filePath: "/download/CustomizableDaysPlayed/v002.mcpack",
+    formatFile: "mcpack",
+  },
+  {
+    id: "/download/customizable-days-played/003-94d188c1-31a7-4476-82b2-5b614bda038c",
+    fileName: "Customizable Days Played v0.0.3",
+    filePath: "/download/CustomizableDaysPlayed/v003.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Day Counter Addon
+  {
+    id: "/download/day-counter-addon/003-df60c0c8-950c-428e-956c-6b4c6e9e3097",
+    fileName: "Day Counter Addon v0.0.3",
+    filePath: "/download/DayCounterAddon/v003.mcaddon",
+    formatFile: "mcaddon",
+  },
+  {
+    id: "/download/day-counter-addon/004-aa390588-c865-4377-948d-c80220e1dc46",
+    fileName: "Day Counter Addon v0.0.4",
+    filePath: "/download/DayCounterAddon/v004.mcaddon",
+    formatFile: "mcaddon",
+  },
+  {
+    id: "/download/day-counter-addon/005-8d594e54-f2ef-4c26-8254-c1a9dfbd75b1",
+    fileName: "Day Counter Addon v0.0.5",
+    filePath: "/download/DayCounterAddon/v005.mcaddon",
+    formatFile: "mcaddon",
+  },
+  
+  
+  // Durability Armor Viewer
+  {
+    id: "/download/durability-armor-viewer/001-fbe71cae-24a5-4200-b80f-ec9246af3dcd",
+    fileName: "Durability Armor Viewer v0.0.1",
+    filePath: "/download/DurabilityArmorViewer/v001.mcaddon",
+    formatFile: "mcaddon",
+  },
+  
+  // Durability Tools Viewer
+  {
+    id: "/download/durability-tools-viewer/003-0c641a1b-34a6-453d-9901-1cb962c76b27",
+    fileName: "Durability Tools Viewer v0.0.3",
+    filePath: "/download/DurabilityToolsViewer/v003.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // F1 Button
+  {
+    id: "/download/f1-button/003-67246598-53a1-412f-a466-2d96ef1941a1",
+    fileName: "F1 Button v0.0.3",
+    filePath: "/download/F1Button/v003.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Nether Coordinate
+  {
+    id: "/download/nether-coordinate/001-995013db-ba7d-47ed-b248-14a2738466c2",
+    fileName: "Nether Coordinate v0.0.1",
+    filePath: "/download/NetherCoordinate/v001.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Ore Info Viewer
+  {
+    id: "/download/ore-info-viewer/002-d199ebd9-3b8e-4048-b440-7a4f8ea6ca8a",
+    fileName: "Ore Info Viewer v0.0.2",
+    filePath: "/download/OreInfoViewer/v002.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Unobstructive Item Name
+  {
+    id: "/download/unobstructive-item-name/001-c2f3340b-fa18-43dd-a11a-e42bbc6ed025",
+    fileName: "Unobstructive Item Name v0.0.1",
+    filePath: "/download/UnobstructiveItemName/v001.mcpack",
+    formatFile: "mcpack",
+  },
+  
+  // Test
+  {
+    id: "/download/test",
+    fileName: "test",
+    filePath: "/download/test.mcpack",
+    formatFile: "mcpack",
+  },
+];
+
+export default downloadLink;
+
+/* cadangan uuid
+
+d45dfd48-9c1e-4d0f-a47a-8db4f62064bc
+
+f7c04723-df84-4f2a-9d31-f78be1bf4d3d
+
+d5a86204-714d-4469-aed5-f80bf33f10c9
+
+5db96ee3-f74d-4c90-ada2-fffd11e1bae0
+
+d5020949-8748-4d38-bb99-f5a36263ecda
+
+deaf637e-0834-477c-92fc-800f2818aac7
+
+94e042c3-949d-46ab-904c-b1b433cd882c
+
+c8c797d7-acdf-4570-8cc4-a8274858118f
+
+691cac1f-9e77-4caf-adf4-a0064b540e56
+
+74c10bdf-fef3-4970-b7af-22d1856e725a
+
+da3d4521-c753-4dab-b075-6838ce08a4e8
+
+2aa4f91a-e89f-4063-8245-9d8c5553e62c
+
+aeb83f0e-8cd0-4713-bfe5-4bdc306688d5
+
+398594a9-96ab-444d-8f93-6c178e3f9a4d
+
+a962e5cc-55c9-4851-8bc1-35d47182c88a
+
+450a8dd7-439a-4214-97ad-858feb66af8a
+
+87279d0c-ccf8-433c-b279-09cde0bc18ee
+
+4af02c22-526e-486a-950c-8a9c543465c9
+
+748f12b7-88f8-43d4-9676-cd895c869c3e
+
+b1d8dc18-8072-4ea2-83ca-2f46b01eefe0
+
+6a290753-46c2-4bb9-8432-d3cd4d3cd8b7
+
+061120a9-e618-4818-820d-8ee02d184ba1
+
+5a655a29-f5e7-407d-90c7-aa999b42e7bf
+
+c246e39b-8405-4369-b7e7-e634e387caf7
+
+a7df9261-260e-4f10-b72b-9b0556fbfeb4
+
+6267da46-07fd-4805-89cc-430cc662c0d1
+
+35720b90-f090-4d47-bed5-61025b0361e0
+
+aaabd29f-c61d-41d5-a191-779cdc43236e
+
+13b2c820-6a01-4933-9a15-8420e790be83
+
+491a3d39-1838-47fb-9084-d6d264c36c3b
+
+6934e501-be0d-4116-a0f3-f0c7eb5e7533
+
+8423689d-61f4-48ab-b6f8-ff50d2c45214
+
+c8ec4485-2e34-46f4-b438-bc4d58292024
+
+d7db5442-207c-43f6-bd5d-856f8bba8ebc
+
+7e0da3bf-51bb-48a7-9ccd-288f1e08ffee
+
+8843dc10-42f3-4385-a326-0b0fe8185f08
+
+2e1b4068-cf83-4e35-85e1-7123d93c01b3
+
+1bc86762-bb1a-4f45-ada1-58926aa4c191
+
+755111b8-4be4-4126-ad9c-7c0d3bfd9abc
+
+3d001087-88bc-46bb-97f9-147b8e47a844
+
+d57d4278-d559-4104-b269-75c7c6589c5c
+
+61b59e59-cf2a-45f8-8087-3426a0c2e1a2
+
+1640a095-d1b0-45d0-9462-a0319fe95b82
+
+1a515075-b09e-4e80-baf9-75ce12f7eced
+
+2e3dfacc-a335-4ba0-b069-ff4f133667b9
+
+2bbee2de-4c4c-4aeb-b1b3-55cb9f50b41b
+
+ff4afc62-879c-43b7-8bc6-afbc96adbb7b
+
+e5e084d9-3c67-47a1-979d-2cf678e26f80
+
+ba13a5ca-0415-44c2-9d10-d702f925df1d
+
+f3442745-3ff1-4847-97b4-3c274a3ea763
+
+6e865ead-6109-424b-b5d1-55ca40556702
+
+ca570105-fef5-49ec-8da7-d99236717fa3
+
+333aaf6e-8376-4b2b-98fe-9dde7e17d1fe
+
+e5db6dbe-75c2-4fe1-8570-4fdf217b65c8
+
+23ab103b-9812-4aa2-9edc-e597b843d477
+
+f411e460-48c2-40d5-8952-dcf5a1207cd6
+
+b070dd5e-e386-4c0d-9d70-7b5bcd762534
+
+56d837ca-03ed-4239-aa07-8d86e74b74a7
+
+67f3bda5-17fb-4ecb-8b27-329a5bc8edc4
+
+1a865ff3-2f1f-47c5-b423-e0ea1b3de517
+
+0685c5fc-cefb-4101-b65e-45ebbaa3a9dd
+
+4371dcfb-e477-4a2e-b624-ce6eb5185152
+
+2d32ce8b-d904-4af4-832c-fdeb32f96cc4
+
+c9bb0fca-eeb2-4bb6-947e-429f914213d9
+
+99b29578-f855-4e3b-9585-97f7ff833108
+
+ba386847-998c-4b4a-a17d-8730b3c108cb
+
+cf87b2ed-02ac-4b19-a16b-d05ebdef0bb9
+
+60c1bb39-6427-4e50-bd09-1b32cd6af83b
+
+6400bbb2-4eee-489c-b110-21b7c6503ce5
+
+1ffce219-489f-487e-9e0f-847147ef1ffd
+
+c013ffbe-e1a8-440e-9005-30ba04358af2
+
+885312c0-832a-47d9-9575-1e75c074e962
+
+d943e81e-e842-4f96-9c01-ee50c5040f8f
+
+2d5b3acb-8a6d-4f0e-9815-4ad4a82f7be9
+
+7596bddc-6d62-4c2b-aa22-8fb030636fec
+
+00893be5-bafa-4174-8ba4-8fa5f4b8a63c
+
+3e09c5be-b957-4d21-b051-b8b5effe40a6
+
+eeaeb1b8-ea40-4510-ac11-37cde614ed70
+
+19ac9260-bc2d-42a1-85b3-c26767c17ab4
+
+2b31b455-9e1e-461a-995e-091fa954b23d
+
+f2cf99fd-5819-4921-90f4-0c34a4b612ef
+
+74685042-99f4-4592-b5a5-0ad7acad4091
+
+1c089878-5094-4816-b5a5-197aa3d89799
+
+be07501c-1bac-4b90-a8da-f75e3550bdf7
+
+cf975c85-7f22-4147-bad2-91386e4819cd
+
+8d7d66be-e9a1-48f7-8d9a-aa3025c0bf5d
+*/
